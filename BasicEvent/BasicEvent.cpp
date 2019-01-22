0521954b0b81df57c2010c7a94349cdac474b002
kaitaocheng <kaitaocheng@ecarx.com.cn>
2019-01-22 15:48:25 +0800

    Add BasicEvent and http-server

---
+++ BasicEvent/BasicEvent.cpp
@@ -0,0 +1,42 @@
+#include "Poco/BasicEvent.h"
+#include "Poco/Delegate.h"
+#include <iostream>
+
+using Poco::BasicEvent;
+using Poco::Delegate;
+
+class Source
+{
+public:
+    BasicEvent<int> theEvent;
+
+    void fireEvent(int n)
+    {
+        theEvent(this, n);
+    }
+};
+
+class Target
+{
+public:
+    void onEvent(const void* pSender, int& arg)
+    {
+        std::cout << "onEvent: " << arg << std::endl;
+    }
+};
+
+int main(int argc, char** argv)
+{
+    Source source;
+    Target target;
+
+    source.theEvent += Delegate<Target, int>(
+        &target, &Target::onEvent);
+
+    source.fireEvent(42);
+
+    source.theEvent -= Delegate<Target, int>(
+        &target, &Target::onEvent);
+
+    return 0;
+}
